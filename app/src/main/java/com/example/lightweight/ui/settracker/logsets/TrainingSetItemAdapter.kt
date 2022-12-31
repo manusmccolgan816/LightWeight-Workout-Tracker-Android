f1515b99bac469c1e52cc8c6a9c137b7b3fa06f5
mjm00070 <mjm00070@students.stir.ac.uk>
2022-12-31 18:53:23 +0000

    Got delete set working

--- app/src/main/java/com/example/lightweight/ui/settracker/logsets/TrainingSetItemAdapter.kt
+++ app/src/main/java/com/example/lightweight/ui/settracker/logsets/TrainingSetItemAdapter.kt
@@ -2,14 +2,20 @@ package com.example.lightweight.ui.settracker.logsets
 
 import android.opengl.Visibility
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lightweight.R
+import com.example.lightweight.data.db.entities.Category
 import com.example.lightweight.data.db.entities.TrainingSet
+import com.example.lightweight.ui.category.ConfirmDeleteCategoryDialog
+import com.example.lightweight.ui.category.EditCategoryDialog
 import com.example.lightweight.ui.trainingset.TrainingSetViewModel
 
 class TrainingSetItemAdapter(
@@ -20,6 +26,7 @@ class TrainingSetItemAdapter(
     private lateinit var parent: ViewGroup
 
     private lateinit var imageViewTrophy: ImageView
+    private lateinit var imageViewSetOptions: ImageView
     private lateinit var textViewSetNumber: TextView
     private lateinit var textViewSetWeight: TextView
     private lateinit var textViewSetReps: TextView
@@ -35,6 +42,7 @@ class TrainingSetItemAdapter(
         val curTrainingSet = trainingSets[position]
 
         imageViewTrophy = holder.itemView.findViewById(R.id.image_view_trophy)
+        imageViewSetOptions = holder.itemView.findViewById(R.id.image_view_training_set_options)
         textViewSetNumber = holder.itemView.findViewById(R.id.text_view_training_set_number)
         textViewSetWeight = holder.itemView.findViewById(R.id.text_view_training_set_weight)
         textViewSetReps = holder.itemView.findViewById(R.id.text_view_training_set_reps)
@@ -45,6 +53,31 @@ class TrainingSetItemAdapter(
         // Display the trophy if the set is a PR
         if (curTrainingSet.isPR) imageViewTrophy.visibility = View.VISIBLE
         else imageViewTrophy.visibility = View.INVISIBLE
+
+        imageViewSetOptions.setOnClickListener {
+            // Create the popup menu anchored to the training set item
+            val popupMenu = PopupMenu(parent.context, holder.itemView, Gravity.END)
+
+            popupMenu.inflate(R.menu.training_set_popup_menu)
+            popupMenu.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.menu_item_edit_training_set -> {
+                        // Display the edit training set dialog
+
+                        true
+                    }
+                    R.id.menu_item_delete_training_set -> {
+                        // Delete the training set
+                        trainingSetViewModel.delete(curTrainingSet)
+                        Toast.makeText(parent.context, "Set has been deleted",
+                            Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> true
+                }
+            }
+            popupMenu.show()
+        }
     }
 
     override fun getItemCount(): Int {
