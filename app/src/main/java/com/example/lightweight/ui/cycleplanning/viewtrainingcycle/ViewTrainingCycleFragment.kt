159ebdaf655214f53e53b2868ca58dfa413ca7eb
mjm00070 <mjm00070@students.stir.ac.uk>
2023-01-26 20:30:02 +0000

    Got CycleDays and CycleDayCategories to display although IOB exception occurs and ordering is not correct

--- app/src/main/java/com/example/lightweight/ui/cycleplanning/viewtrainingcycle/ViewTrainingCycleFragment.kt
+++ app/src/main/java/com/example/lightweight/ui/cycleplanning/viewtrainingcycle/ViewTrainingCycleFragment.kt
@@ -46,7 +46,9 @@ class ViewTrainingCycleFragment : Fragment(R.layout.fragment_view_training_cycle
     private var cycleID: Int? = null
     private lateinit var category: Category
 
-    private var cycleDaysAndCategories: ArrayList<ArrayList<CycleDay>> = ArrayList()
+    private var adapters: ArrayList<TrainingCycleDayAdapter1> = ArrayList()
+
+    private var items = arrayListOf<Int>()
 
     private lateinit var recyclerViewTrainingCycleDays: RecyclerView
     private lateinit var fabAddTrainingCycleDay: FloatingActionButton
@@ -69,37 +71,104 @@ class ViewTrainingCycleFragment : Fragment(R.layout.fragment_view_training_cycle
         recyclerViewTrainingCycleDays = view.findViewById(R.id.recycler_view_training_cycle_days)
         fabAddTrainingCycleDay = view.findViewById(R.id.fab_add_training_cycle_day)
 
-        val cycleDayAdapter = TrainingCycleDayAdapter(
+//        val cycleDayAdapter = TrainingCycleDayAdapter(
+//            listOf(),
+//            fun(category: Category) { this.category = category },
+//            this
+//        )
+        val cycleDayAdapter1: TrainingCycleDayAdapter1 = TrainingCycleDayAdapter1(
+            arrayListOf(),
             listOf(),
             fun(category: Category) { this.category = category },
+            mapOf(),
+            TrainingCycleDayAdapter1.LAYOUT_CYCLE_DAY,
             this
         )
+        adapters.add(cycleDayAdapter1)
         val cycleDayCategoryAdapter = TrainingCycleDayCategoryAdapter(mapOf(), this)
-        val concatAdapter = ConcatAdapter(cycleDayAdapter, cycleDayCategoryAdapter)
+        //val concatAdapter = ConcatAdapter(cycleDayAdapter, cycleDayCategoryAdapter)
 
         recyclerViewTrainingCycleDays.layoutManager = LinearLayoutManager(requireContext())
-        recyclerViewTrainingCycleDays.adapter = concatAdapter
+        recyclerViewTrainingCycleDays.adapter = cycleDayAdapter1
+//        recyclerViewTrainingCycleDays.adapter = concatAdapter
 
         cycleDayViewModel.getCycleDaysOfCycle(cycleID).observe(viewLifecycleOwner) { cycleDays ->
-            cycleDayAdapter.cycleDays = cycleDays
-            cycleDayAdapter.notifyDataSetChanged()
-            Log.d(logTag, "cycleDayAdapter data set changed")
+            for (i in cycleDays.indices) {
+                if (cycleDayAdapter1.items.size <= i) {
+                    cycleDayAdapter1.items.add(TrainingCycleDayAdapter1.LAYOUT_CYCLE_DAY)
+                    Log.d(logTag, "items.add: 0")
+                    //cycleDayAdapter1.notifyItemInserted(cycleDayAdapter1.items.size + i)
+                }
+            }
+            items = cycleDayAdapter1.items
+            cycleDayAdapter1.cycleDays = cycleDays
+            cycleDayAdapter1.notifyDataSetChanged()
+            Log.d(logTag, "cycleDayAdapter1 data set changed")
 
             for (cycleDay in cycleDays) {
                 cycleDayCategoryViewModel.getCycleDayCategoriesAndNamesOfCycleDay(cycleDay.cycleDayID)
                     .observe(viewLifecycleOwner) { idNameMappings ->
-                        cycleDayCategoryAdapter.idNameMappings = idNameMappings
-                        cycleDayCategoryAdapter.notifyDataSetChanged()
-                        Log.d(logTag, "cycleDayCategoryAdapter data set changed")
+                        if (idNameMappings.isNotEmpty()) {
+                            for (i in idNameMappings.values.toTypedArray().indices) {
+                                cycleDayAdapter1.items.add(TrainingCycleDayAdapter1.LAYOUT_CYCLE_DAY_CAT)
+                                Log.d(logTag, "items.add: 1")
+//                                if (cycleDayAdapter1.items.size <= i) {
+//                                    cycleDayAdapter1.items.add(TrainingCycleDayAdapter1.LAYOUT_CYCLE_DAY_CAT)
+//                                    Log.d(logTag, "items.add: 1")
+//                                }
+                            }
+                            items = cycleDayAdapter1.items
+                            cycleDayAdapter1.idNameMappings = idNameMappings
+
+                            cycleDayAdapter1.notifyDataSetChanged()
+
+                            Log.d(
+                                logTag,
+                                "cda1 data set changed, idNameMappings size: ${idNameMappings.size}"
+                            )
+//                        adapters.add(cda1)
+//                        recyclerViewTrainingCycleDays.adapter = ConcatAdapter(adapters)
+                        }
                     }
             }
         }
+
+//        cycleDayViewModel.getCycleDaysOfCycle(cycleID).observe(viewLifecycleOwner) { cycleDays ->
+//            cycleDayAdapter.cycleDays = cycleDays
+//            cycleDayAdapter.notifyDataSetChanged()
+//            Log.d(logTag, "cycleDayAdapter data set changed")
+//
+//            for (cycleDay in cycleDays) {
+//                cycleDayCategoryViewModel.getCycleDayCategoriesAndNamesOfCycleDay(cycleDay.cycleDayID)
+//                    .observe(viewLifecycleOwner) { idNameMappings ->
+//                        cycleDayCategoryAdapter.idNameMappings = idNameMappings
+//                        cycleDayCategoryAdapter.notifyDataSetChanged()
+//                        Log.d(logTag, "cycleDayCategoryAdapter data set changed")
+//
+//                        adapters = arrayListOf()
+//
+//                        adapters.add(
+//                            TrainingCycleDayAdapter1(
+//                                cycleDays,
+//                                fun(category: Category) { this.category = category },
+//                                idNameMappings,
+//                                this
+//                            ) as RecyclerView.Adapter<RecyclerView.ViewHolder>
+//                        )
+//
+////                        val concatAdapterConfig =
+////                            ConcatAdapter.Config.Builder().setIsolateViewTypes(false).build()
+//                        recyclerViewTrainingCycleDays.adapter =
+//                            ConcatAdapter(adapters)
+//                    }
+//            }
+//        }
 
         fabAddTrainingCycleDay.setOnClickListener {
             AddTrainingCycleDayDialog(
                 requireContext(),
                 cycleID,
-                cycleDayAdapter.itemCount,
+                cycleDayAdapter1.itemCount, // TODO Ensure this is changed
                 fun(cycleDay: CycleDay) { cycleDayViewModel.insert(cycleDay) }
             ).show()
         }
